1d389a03276e535ef97dea9e3d18072d78fb2864
bengetacademy <ben@getacademy.no>
2022-01-05 10:54:20 +0100

    Added Mouse Over Event to Javascript Page

--- Moodle/Js/eventListeners.js
+++ Moodle/Js/eventListeners.js
@@ -1,14 +1,13 @@
 const cube = document.getElementById("double-click");
 const contextMenu = document.getElementById("context-menu");
 const keyDown = document.getElementById("key-down");
+const table = document.getElementById("table").children[0];
 
 cube.addEventListener("dblclick", function () {
 	let currentCubeColor = getComputedStyle(cube).backgroundColor;
 	let cubeSecondColor = "rgb(255, 255, 255)";
 
 	cube.style.backgroundColor = currentCubeColor !== cubeSecondColor ? cubeSecondColor : "rgb(194, 14, 14)";
-
-	console.log("test");
 });
 
 contextMenu.addEventListener("contextmenu", function () {
@@ -19,3 +18,14 @@ keyDown.addEventListener("keydown", function (e) {
 	let keyElement = keyDown.children[0];
 	keyElement.innerHTML = e.key;
 });
+
+for (let i = 0; i < table.children.length; i++) {
+	const row = table.children[i];
+	console.log(row);
+	for (let i = 0; i < row.children.length; i++) {
+		const cell = row.children[i];
+		cell.addEventListener("mouseover", function () {
+			this.innerText = this.innerText == "o" ? "x" : "o";
+		});
+	}
+}
