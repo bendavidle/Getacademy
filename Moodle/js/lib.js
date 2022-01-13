0a23b5ee247ac1a0a91d5c6f83236f86534248ac
bengetacademy <ben@getacademy.no>
2022-01-13 10:43:58 +0100

    Unit testing

---
+++ Moodle/js/lib.js
@@ -0,0 +1,24 @@
+function fixText(text) {
+	let newText = text.replaceAll(" ", "").toLowerCase();
+	return newText.charAt(0).toUpperCase() + newText.slice(1);
+}
+
+function checkIfEmail(email) {
+	if (email) {
+		if (email.includes(" ")) {
+			return false;
+		} else if (email.includes("@")) {
+			if (email.includes(".")) {
+				if (email.charAt(email.indexOf("@") - 1) == "." || email.charAt(email.indexOf("@") + 1) == ".") {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
